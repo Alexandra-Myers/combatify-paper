403994ff5c5290223c52a2df7b84405d74093524
Infinityy <120841985+Inf1nityy@users.noreply.github.com>
2024-11-21 03:22:24 +0100

    update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,6 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
 
-rootProject.name = "forktest"
+rootProject.name = "combatify"
 
-include("forktest-api", "forktest-server")
+include("combatify-api", "combatify-server")
